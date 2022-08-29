cecbe43d195e413d47967353cbb28705d1b3ad67
Min Jun Kim <eric@itsmnjn.com>
2022-08-29 03:30:39 -0400

    remove unnecessary files

--- next-sitemap.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteUrl: 'https://ts-nextjs-tailwind-starter.theodorusclarence.com/',
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
-  },
-};
